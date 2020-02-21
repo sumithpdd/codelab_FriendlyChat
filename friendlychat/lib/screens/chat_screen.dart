f0e74c2a7294b579d5616f3d5833a6ffdefa56d6
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-02-21 15:38:31 +0100

    Add a UI for composing messages

--- friendlychat/lib/screens/chat_screen.dart
+++ friendlychat/lib/screens/chat_screen.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Friendlychat")),
+      body: _buildTextComposer(),
     );
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor), //new
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0), //new
+              child: new IconButton(
+                  icon: new Icon(Icons.send), //new
+                  onPressed: () =>
+                      _handleSubmitted(_textController.text)), //new
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
   }
 }
